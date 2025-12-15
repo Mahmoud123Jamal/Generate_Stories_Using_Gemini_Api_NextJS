01551dd1d9e26a570e18f188cf259c132e35c2c5
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-15 11:18:19 +0200

    refactor Loading component for improved layout and naming consistency

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,5 +1,9 @@
-function loading() {
-  return <span className="loading loading-spinner text-warning"></span>;
+function Loading() {
+  return (
+    <div className="h-screen grid place-content-center">
+      <span className="loading loading-spinner text-warning loading-xl"></span>
+    </div>
+  );
 }
 
-export default loading;
+export default Loading;
