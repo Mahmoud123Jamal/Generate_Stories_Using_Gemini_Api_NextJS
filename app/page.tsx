e04e6b2e62aa666f3477e49a755f74671b8b7379
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-15 11:18:38 +0200

    add dynamic loading for LandingPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Header from "@/components/Header";
+import Loading from "@/components/loading";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const LandingPage = dynamic(() => import("@/components/LandingPage"), {
+    loading: () => <Loading />,
+  });
   return (
     <div>
       <Header />
+      <LandingPage />
     </div>
   );
 }
