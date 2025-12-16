add771680c70a1de2571ce55644bbf85b8cbfc69
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-16 10:41:21 +0200

    feat: replace Provider with AuthProvider for improved authentication handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Provider from "../components/providers/Provider";
+import AuthProvider from "../components/providers/AuthProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,7 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Provider>{children}</Provider>
+          <AuthProvider>{children}</AuthProvider>
         </body>
       </html>
     </ClerkProvider>
