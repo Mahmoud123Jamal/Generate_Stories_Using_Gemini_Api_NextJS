79f035816c05b0b3587f078931f3855679ac916b
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-16 09:26:50 +0200

    move  Header, LandingPage, and Loading components

--- components/loading.tsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-  return (
-    <div className="h-screen grid place-content-center">
-      <span className="loading loading-spinner text-warning loading-xl"></span>
-    </div>
-  );
-}
-
-export default Loading;
