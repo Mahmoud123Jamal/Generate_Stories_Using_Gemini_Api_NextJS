7195df4062b0f9663257969bc379aa960722d1ad
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-16 10:40:41 +0200

    refactor: rename API route and Provider component

--- components/providers/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type providerProps = {
-  children: React.ReactNode;
-};
-function Provider({ children }: providerProps) {
-  return <div>{children}</div>;
-}
-
-export default Provider;
