65bc2d04cfcc86f0fa68e83b1feb8e60530bf502
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-16 23:37:39 +0200

    feat: create Dashboardpage component with Banner and StoryList

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Banner from "@/components/ui/Banner";
+import StoryList from "@/components/ui/StoryList";
+
+function Dashboardpage() {
+  return (
+    <>
+      <Banner />
+      <StoryList />
+    </>
+  );
+}
+
+export default Dashboardpage;
