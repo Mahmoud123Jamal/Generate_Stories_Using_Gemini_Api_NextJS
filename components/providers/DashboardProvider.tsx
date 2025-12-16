051b0a604b74d7bd327df00a96921ecb6703d28a
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-16 23:29:23 +0200

    feat: wrap children with AppSideBar in DashboardProvider

--- components/providers/DashboardProvider.tsx
+++ components/providers/DashboardProvider.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import AppSideBar from "../ui/AppSideBar";
 
 type Props = {
   children: React.ReactNode;
 };
 function DashboardProvider({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <AppSideBar>{children}</AppSideBar>
+    </div>
+  );
 }
 
 export default DashboardProvider;
