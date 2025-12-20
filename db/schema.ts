7a6004b18db0aa6391af16c250d56f9b43a3f634
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-20 15:59:19 +0200

    add storysTable

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,18 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const storysTable = pgTable("storys", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  storyId: varchar().notNull().unique(),
+  subject: varchar().notNull(),
+  type: varchar().notNull(),
+  ageGroup: varchar().notNull(),
+  imageUrl: varchar().default(""),
+  content: json(),
+  email: varchar("email").references(() => usersTable.email),
 });
