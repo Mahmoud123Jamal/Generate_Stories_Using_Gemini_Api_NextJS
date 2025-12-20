3680791b0e558b6e65de325bd7358ba9f8e7eb0a
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-20 16:26:55 +0200

    Add my_stories Api logic

---
+++ app/api/my-stories/route.ts
@@ -0,0 +1,19 @@
+import db from "@/db";
+import { storysTable } from "@/db/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const user = await currentUser();
+
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const stories = await db
+    .select()
+    .from(storysTable)
+    .where(eq(storysTable.email, user.primaryEmailAddress?.emailAddress!));
+  return NextResponse.json({ stories });
+}
