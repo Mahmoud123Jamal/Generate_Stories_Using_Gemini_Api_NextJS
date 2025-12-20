f3196df4c18c7242df2c94f1b719c83fab0a86d7
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-20 17:13:52 +0200

    schema type safeyy

--- types/StoryType.ts
+++ types/StoryType.ts
@@ -1,3 +1,5 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+
 export const STORY_TYPES = [
   {
     key: "story book",
@@ -17,9 +19,20 @@ export const STORY_TYPES = [
 ] as const;
 
 export type StoryType = (typeof STORY_TYPES)[number]["key"];
+//type for schema
+export const STORY_TYPE_VALUES = STORY_TYPES.map((t) => t.key);
+export const storyTypeEnum = pgEnum(
+  "story_type",
+  STORY_TYPE_VALUES as [StoryType, ...StoryType[]]
+);
 
 export const AGE_GROUPS = ["2-5", "5-7", "7-10"] as const;
 export type AgeGroup = (typeof AGE_GROUPS)[number];
+// Age for schema
+export const ageGroupEnum = pgEnum("age_group", [...AGE_GROUPS] as [
+  AgeGroup,
+  ...AgeGroup[]
+]);
 
 export type StoryFormValues = {
   subject: string;
