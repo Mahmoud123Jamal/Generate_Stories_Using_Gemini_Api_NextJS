ef77de30e05c9ee48f676136c913738a7fcb498c
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-21 09:12:56 +0200

    create loader component

---
+++ components/ui/loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="h-screen grid place-content-center">
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+}
+
+export default Loader;
