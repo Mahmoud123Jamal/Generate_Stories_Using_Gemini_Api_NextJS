c5f024fb2473582a78ce818e2a589b251772f501
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-21 09:11:20 +0200

    update user type

--- types/UserTable.ts
+++ types/UserTable.ts
@@ -1,5 +1,4 @@
 import { usersTable } from "@/db/schema";
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { InferSelectModel } from "drizzle-orm";
 
 export type User = InferSelectModel<typeof usersTable>;
-export type InsertUser = InferInsertModel<typeof usersTable>;
