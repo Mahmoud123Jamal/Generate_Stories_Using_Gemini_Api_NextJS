16012bc07674d5104687093c4e94a964c3921118
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-22 14:28:26 +0200

    create all-stories route to preview all stories

---
+++ app/api/all-stories/route.ts
@@ -0,0 +1,21 @@
+import db from "@/db";
+import { storysTable } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const stories = await db
+      .select()
+      .from(storysTable)
+      .orderBy(desc(storysTable.id));
+
+    return NextResponse.json(stories);
+  } catch (error) {
+    console.error("Error fetching general stories:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
