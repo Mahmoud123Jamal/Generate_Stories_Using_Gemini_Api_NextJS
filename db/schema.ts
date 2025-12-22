5fd377e795f1843b63444d9fbf0b3122b0172612
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-22 15:48:01 +0200

    fix schema types and structure

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,3 @@
-import { ageGroupEnum, storyTypeEnum } from "@/types/StoryType";
 import { integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
@@ -9,11 +8,13 @@ export const usersTable = pgTable("users", {
 
 export const storysTable = pgTable("storys", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  storyId: varchar().notNull().unique(),
-  subject: varchar().notNull(),
-  type: storyTypeEnum("type").notNull(),
-  ageGroup: ageGroupEnum().notNull(),
-  imageUrl: varchar().default(""),
-  content: json(),
-  email: varchar("email").references(() => usersTable.email),
+  storyId: varchar("storyId", { length: 255 }).notNull().unique(),
+  subject: varchar("subject", { length: 255 }).notNull(),
+
+  type: varchar("type", { length: 50 }).notNull(),
+  ageGroup: varchar("ageGroup", { length: 50 }).notNull(),
+
+  imageUrl: varchar("imageUrl", { length: 500 }).default(""),
+  content: json("content"),
+  email: varchar("email", { length: 255 }).references(() => usersTable.email),
 });
--- types/StoryType.ts
+++ types/StoryType.ts
@@ -1,5 +1,3 @@
-import { pgEnum } from "drizzle-orm/pg-core";
-
 export const STORY_TYPES = [
   {
     key: "story book",
@@ -19,20 +17,9 @@ export const STORY_TYPES = [
 ] as const;
 
 export type StoryType = (typeof STORY_TYPES)[number]["key"];
-//type for schema
-export const STORY_TYPE_VALUES = STORY_TYPES.map((t) => t.key);
-export const storyTypeEnum = pgEnum(
-  "story_type",
-  STORY_TYPE_VALUES as [StoryType, ...StoryType[]]
-);
 
 export const AGE_GROUPS = ["2-5", "5-7", "7-10"] as const;
 export type AgeGroup = (typeof AGE_GROUPS)[number];
-// Age for schema
-export const ageGroupEnum = pgEnum("age_group", [...AGE_GROUPS] as [
-  AgeGroup,
-  ...AgeGroup[]
-]);
 
 export type StoryFormValues = {
   subject: string;
